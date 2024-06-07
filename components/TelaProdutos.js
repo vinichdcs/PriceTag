3d9cd7c683de7cf537b8c1ec57d81d7025694bed
Vinicius Sanches <126787332+vinichdcs@users.noreply.github.com>
2024-06-07 17:57:04 -0300

    Update TelaProdutos.js
    Implementado CRUD com persistência dos dados. Agora o App Price Tag tem as opções de criar, editar, excluir e listar os produtos.

--- components/TelaProdutos.js
+++ components/TelaProdutos.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Alert, TextInput, Modal } from 'react-native';
 import firebase from '../config/Firebase';
 
 const TelaProdutos = () => {
   const [produtos, setProdutos] = useState([]);
   const [produtoSelecionado, setProdutoSelecionado] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [produtoEditado, setProdutoEditado] = useState({
+    nomeComercio: '',
+    endereco: '',
+    nomeProduto: '',
+    categoriaProduto: '',
+    marcaProduto: '',
+    unidadeMedida: '',
+    valorProduto: ''
+  });
 
   useEffect(() => {
     const carregarProdutos = async () => {
@@ -32,13 +42,56 @@ const TelaProdutos = () => {
       const produtosAtualizados = produtos.filter((produto) => produto.id !== produtoSelecionado.id);
       setProdutos(produtosAtualizados);
       setProdutoSelecionado(null);
+      Alert.alert('Sucesso', 'Produto excluído com sucesso!');
     } catch (error) {
       console.error('Erro ao excluir o produto:', error);
     }
   };
 
   const handleSelecionarProduto = (produto) => {
-    setProdutoSelecionado(produto);
+    if (produtoSelecionado && produtoSelecionado.id === produto.id) {
+      setProdutoSelecionado(null);
+    } else {
+      setProdutoSelecionado(produto);
+    }
+  };
+
+  const handleAbrirModal = () => {
+    setProdutoEditado(produtoSelecionado);
+    setModalVisible(true);
+  };
+
+  const handleConfirmarAlteracao = () => {
+    Alert.alert(
+      'Confirmar Alteração',
+      'Tem certeza de que deseja alterar este produto?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Alterar',
+          onPress: handleAlterarProduto,
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const handleAlterarProduto = async () => {
+    try {
+      await firebase.database().ref('produtos').child(produtoSelecionado.id).update(produtoEditado);
+      const produtosAtualizados = produtos.map((produto) =>
+        produto.id === produtoSelecionado.id ? { id: produtoSelecionado.id, ...produtoEditado } : produto
+      );
+      setProdutos(produtosAtualizados);
+      setProdutoSelecionado({ id: produtoSelecionado.id, ...produtoEditado });
+      setModalVisible(false);
+      Alert.alert('Sucesso', 'Produto alterado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao alterar o produto:', error);
+    }
   };
 
   const confirmarExclusao = () => {
@@ -67,12 +120,25 @@ const TelaProdutos = () => {
       <Text style={styles.texto}>{`Nome do Comércio: ${item.nomeComercio}`}</Text>
       <Text style={styles.texto}>{`Nome do Produto: ${item.nomeProduto}`}</Text>
       {item.id === (produtoSelecionado && produtoSelecionado.id) && (
-        <TouchableOpacity
-          style={styles.botaoExcluir}
-          onPress={confirmarExclusao}
-        >
-          <Text style={styles.textoBotaoExcluir}>Excluir</Text>
-        </TouchableOpacity>
+        <View>
+          <Text style={styles.texto}>{`Endereço: ${item.endereco}`}</Text>
+          <Text style={styles.texto}>{`Categoria do Produto: ${item.categoriaProduto}`}</Text>
+          <Text style={styles.texto}>{`Marca do Produto: ${item.marcaProduto}`}</Text>
+          <Text style={styles.texto}>{`Unidade de Medida: ${item.unidadeMedida}`}</Text>
+          <Text style={styles.texto}>{`Valor do Produto: ${item.valorProduto}`}</Text>
+          <TouchableOpacity
+            style={styles.botaoExcluir}
+            onPress={confirmarExclusao}
+          >
+            <Text style={styles.textoBotaoExcluir}>Excluir</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.botaoAlterar}
+            onPress={handleAbrirModal}
+          >
+            <Text style={styles.textoBotaoAlterar}>Alterar</Text>
+          </TouchableOpacity>
+        </View>
       )}
     </TouchableOpacity>
   );
@@ -87,6 +153,72 @@ const TelaProdutos = () => {
           renderItem={renderItem}
           keyExtractor={(item) => item.id}
         />
+      )}
+
+      {modalVisible && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.input}
+              placeholder="Nome do Comércio"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, nomeComercio: text })}
+              value={produtoEditado.nomeComercio}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Endereço"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, endereco: text })}
+              value={produtoEditado.endereco}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Nome do Produto"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, nomeProduto: text })}
+              value={produtoEditado.nomeProduto}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Categoria do Produto"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, categoriaProduto: text })}
+              value={produtoEditado.categoriaProduto}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Marca do Produto"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, marcaProduto: text })}
+              value={produtoEditado.marcaProduto}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Unidade de Medida"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, unidadeMedida: text })}
+              value={produtoEditado.unidadeMedida}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Valor do Produto"
+              onChangeText={(text) => setProdutoEditado({ ...produtoEditado, valorProduto: text })}
+              value={produtoEditado.valorProduto}
+            />
+            <TouchableOpacity
+              style={[styles.botaoExcluir, styles.botaoAlterar]}
+              onPress={handleConfirmarAlteracao}
+            >
+              <Text style={styles.textoBotaoAlterar}>Salvar Alterações</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.botaoCancelar}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.textoBotaoCancelar}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
       )}
     </View>
   );
@@ -123,6 +255,45 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'center',
   },
+  botaoAlterar: {
+    backgroundColor: '#ffc107',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  textoBotaoAlterar: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+    margin: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  botaoCancelar: {
+    backgroundColor: '#6c757d',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  textoBotaoCancelar: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
 
 export default TelaProdutos;
+
